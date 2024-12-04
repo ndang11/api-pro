de93089dc20b2394f9d7d9297061c3e7452c7372
ndang.royalty <royaltyhopsin@gmail.com>
2024-12-04 10:47:13 +0100

    pushing

--- script/api.js
+++ script/api.js
@@ -1,13 +1,13 @@
 fetch('https://www.swapi.tech/api/films')
-.then((res) => res.json())
-.then((data) => {
-  console.log(data)
-  let titleOne = document.getElementById("paragraph-one")
-  let titleTwo = document.getElementById("paragraph-two" )
-  let titleThree = document.getElementById("paragraph-three" )
-  let titleFour = document.getElementById("paragraph-four" )
-  let titleFive = document.getElementById( "paragraph-five")
-  let titleSix = document.getElementById("paragraph-six")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+    const titleOne = document.getElementById("paragraph-one")
+    const titleTwo = document.getElementById("paragraph-two" )
+    const titleThree = document.getElementById("paragraph-three" )
+    const titleFour = document.getElementById("paragraph-four" )
+    const titleFive = document.getElementById( "paragraph-five")
+    const titleSix = document.getElementById("paragraph-six")
   for (let index = 0; index < data.result.length; index++) {
     titleOne.innerHTML = data.result[0].properties.title
     titleTwo.innerHTML = data.result[1].properties.title
@@ -16,4 +16,4 @@ fetch('https://www.swapi.tech/api/films')
     titleFive.innerHTML = data.result[4].properties.title
     titleSix.innerHTML = data.result[5].properties.title
   }
-})
+  })
