282e6fbc35e785bb2f4fd8bafc144a7ce1177b00
ndang.royalty <royaltyhopsin@gmail.com>
2024-12-04 10:56:06 +0100

    solving linters

--- script/api.js
+++ script/api.js
@@ -2,18 +2,18 @@ fetch('https://www.swapi.tech/api/films')
   .then((res) => res.json())
   .then((data) => {
     console.log(data)
-    const titleOne = document.getElementById("paragraph-one")
-    const titleTwo = document.getElementById("paragraph-two" )
-    const titleThree = document.getElementById("paragraph-three" )
-    const titleFour = document.getElementById("paragraph-four" )
-    const titleFive = document.getElementById( "paragraph-five")
+    const titleOne = document.getElementById('paragraph-one')
+    const titleTwo = document.getElementById('paragraph-two')
+    const titleThree = document.getElementById('paragraph-three')
+    const titleFour = document.getElementById('paragraph-four')
+    const titleFive = document.getElementById('paragraph-five')
     const titleSix = document.getElementById("paragraph-six")
-  for (let index = 0; index < data.result.length; index++) {
-    titleOne.innerHTML = data.result[0].properties.title
-    titleTwo.innerHTML = data.result[1].properties.title
-    titleThree.innerHTML = data.result[2].properties.title
-    titleFour.innerHTML = data.result[3].properties.title
-    titleFive.innerHTML = data.result[4].properties.title
-    titleSix.innerHTML = data.result[5].properties.title
-  }
+    for (let index = 0; index < data.result.length; index++) {
+      tleOne.innerHTML = data.result[0].properties.title
+      tleTwo.innerHTML = data.result[1].properties.title
+      tleThree.innerHTML = data.result[2].properties.title
+      tleFour.innerHTML = data.result[3].properties.title
+      tleFive.innerHTML = data.result[4].properties.title
+      titleSix.innerHTML = data.result[5].properties.title
+    }
   })
